d49a4784b9673e9a307e8599b459d9f8df885baa
Alex5HTML <Metal2079@yandex.ru>
2018-02-05 20:33:03 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Calculate
+* Class prints "Hello World"
+*@author Alexandr Sedykh
+*@since 05.02.2018
+*/
+
 public class Calculate{
+	  /**
+	  *Main
+	  *@param args - args
+	  */
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexandr Sedykh
+* @version $Id$
+* @since 05.02.2018
+*/
+package ru.job4j;
