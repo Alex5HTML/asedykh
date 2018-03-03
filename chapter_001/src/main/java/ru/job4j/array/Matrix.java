2ba5ddc72b8fff2c738908d7e17bea2a773eaeb7
Alex5HTML <Metal2079@yandex.ru>
2018-03-03 17:29:58 +0400

    5.4. Двухмерный массив. Таблица умножения. [#33491], исправленое

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -14,16 +14,10 @@ public class Matrix {
 	public int[][] multiple(int size) {
 		int i = 0;
 		int j = 0;
-		int[][] array = new int[size + 1][size + 1];
-		for (i = 0; i <= size; i++) {
-			for (j = 0; j <= size; j++) {
-				if (i == 0 && j != 0) {
-					array[i][j] = j;
-				} else if (i != 0 && j == 0) {
-					array[i][j] = i;
-				} else {
-					array[i][j] = i * j;
-				}
+		int[][] array = new int[size][size];
+		for (i = 0; i < size; i++) {
+			for (j = 0; j < size; j++) {
+				array[i][j] = (i + 1) * (j + 1);
 			}
 		}
 		return array;
--- chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -10,10 +10,9 @@ public class MatrixTest {
 		Matrix matrix = new Matrix();
 		int[][] resultArray = matrix.multiple(3);
 		int[][] expectArray = {
-							   {0, 1, 2, 3},
-							   {1, 1, 2, 3},
-							   {2, 2, 4, 6},
-							   {3, 3, 6, 9}
+							   {1, 2, 3},
+							   {2, 4, 6},
+							   {3, 6, 9}
 							   };
 		assertThat(resultArray, is(expectArray));
 	}
@@ -22,12 +21,11 @@ public class MatrixTest {
 		Matrix matrix = new Matrix();
 		int[][] resultArray = matrix.multiple(5);
 		int[][] expectArray = {
-							  {0, 1, 2, 3, 4, 5},
-							  {1, 1, 2, 3, 4, 5},
-							  {2, 2, 4, 6, 8, 10},
-						      {3, 3, 6, 9, 12, 15},
-							  {4, 4, 8, 12, 16, 20},
-							  {5, 5, 10, 15, 20, 25}
+							  {1, 2, 3, 4, 5},
+							  {2, 4, 6, 8, 10},
+						      {3, 6, 9, 12, 15},
+							  {4, 8, 12, 16, 20},
+							  {5, 10, 15, 20, 25}
 						      };
 	}
 }
