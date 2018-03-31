745e4bd1a8d8d975187ca9dd8264ddbe78e6441a
Alex5HTML <Metal2079@yandex.ru>
2018-03-31 14:12:59 +0400

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,10 +1,129 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+	
+	private final PrintStream stdout = System.out;
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+	
+	@Before
+	public void loadOutput() {
+		System.setOut(new PrintStream(this.out));
+	}
+	
+	@After
+	public void backOutput() {
+		System.setOut(this.stdout);
+	}
+	
+	@Test
+	public void whenShowAllItemsThenTrackerDisplayItems() {
+		Tracker tracker = new Tracker();
+		Item item = tracker.add(new Item("test name", "desc", 123L));
+		Input input = new StubInput(new String[]{"1", "6"});
+		new StartUI(input, tracker).init();
+		assertThat(new String(this.out.toByteArray()),
+										is(new StringBuilder()
+										.append("Меню.")
+										.append(System.lineSeparator())
+										.append("0 - Добавить новую заявку")
+										.append(System.lineSeparator())
+										.append("1 - Показать список всех заявок")
+										.append(System.lineSeparator())
+										.append("2 - Редактировать заявку")
+										.append(System.lineSeparator())
+										.append("3 - Удалить заявку")
+										.append(System.lineSeparator())
+										.append("4 - Найти заявку по ID")
+										.append(System.lineSeparator())
+										.append("5 - Найти заявку по имени")
+										.append(System.lineSeparator())
+										.append("6 - Выход")
+										.append(System.lineSeparator())
+										.append("---------- Список всех заявок ----------")
+										.append(System.lineSeparator())
+										.append("---------- Имя заявки: test name ----------")
+										.append(System.lineSeparator())
+										.append("---------- Описание: " + item.getDescription() + " ----------")
+										.append(System.lineSeparator())
+										.append("---------- ID заявки: " + item.getId() + " ----------")
+										.append(System.lineSeparator())
+										.append("Меню.")
+										.append(System.lineSeparator())
+										.append("0 - Добавить новую заявку")
+										.append(System.lineSeparator())
+										.append("1 - Показать список всех заявок")
+										.append(System.lineSeparator())
+										.append("2 - Редактировать заявку")
+										.append(System.lineSeparator())
+										.append("3 - Удалить заявку")
+										.append(System.lineSeparator())
+										.append("4 - Найти заявку по ID")
+										.append(System.lineSeparator())
+										.append("5 - Найти заявку по имени")
+										.append(System.lineSeparator())
+										.append("6 - Выход")
+										.append(System.lineSeparator())
+										.toString()
+										));
+	}
+	
+	@Test
+	public void whenDeleteItemThenTrackerDisplayDeleteItem() {
+		Tracker tracker = new Tracker();
+		Item item = tracker.add(new Item("test name", "desc", 123L));
+		Item item1 = tracker.add(new Item("test name1", "desc", 1234L));
+		Input input = new StubInput(new String[]{"3", item.getId(), "6"});
+		new StartUI(input, tracker).init();
+		assertThat(new String(this.out.toByteArray()),
+										is(new StringBuilder()
+										.append("Меню.")
+										.append(System.lineSeparator())
+										.append("0 - Добавить новую заявку")
+										.append(System.lineSeparator())
+										.append("1 - Показать список всех заявок")
+										.append(System.lineSeparator())
+										.append("2 - Редактировать заявку")
+										.append(System.lineSeparator())
+										.append("3 - Удалить заявку")
+										.append(System.lineSeparator())
+										.append("4 - Найти заявку по ID")
+										.append(System.lineSeparator())
+										.append("5 - Найти заявку по имени")
+										.append(System.lineSeparator())
+										.append("6 - Выход")
+										.append(System.lineSeparator())
+										.append("---------- Удаление заявки ----------")
+										.append(System.lineSeparator())
+										.append("---------- Заявка " + item.getId() + " удалена ----------")
+										.append(System.lineSeparator())
+										.append("Меню.")
+										.append(System.lineSeparator())
+										.append("0 - Добавить новую заявку")
+										.append(System.lineSeparator())
+										.append("1 - Показать список всех заявок")
+										.append(System.lineSeparator())
+										.append("2 - Редактировать заявку")
+										.append(System.lineSeparator())
+										.append("3 - Удалить заявку")
+										.append(System.lineSeparator())
+										.append("4 - Найти заявку по ID")
+										.append(System.lineSeparator())
+										.append("5 - Найти заявку по имени")
+										.append(System.lineSeparator())
+										.append("6 - Выход")
+										.append(System.lineSeparator())
+										.toString()
+										));
+	}
+	
 	@Test
 	public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
 		Tracker tracker = new Tracker();
